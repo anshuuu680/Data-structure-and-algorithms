56c275f169bf1bd246e0202a53b5f01787ff357b
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-04-27 15:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (93.41%) - LeetSync

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+
+  void solve(string digits,string output,int index,vector<string>&ans,string mapping[]){
+      // Base case
+        if(index>=digits.size()){
+            if(output.length()>0)
+            ans.push_back(output);
+            return;
+        } 
+    
+      int number=digits[index]-'0';
+      string value=mapping[number];
+
+      for(int i=0;i<value.length();i++){
+          output.push_back(value[i]);
+         solve(digits,output,index+1,ans,mapping);
+       output.pop_back();
+      }
+  }
+
+
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        vector<string>ans;
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        solve(digits,"",0,ans,mapping);
+
+        return ans;
+
+    }
+};
