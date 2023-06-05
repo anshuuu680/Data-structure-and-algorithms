677809e9c11eb491ad7dc7312af2ee93fc14cd4a
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-05 14:22:39 +0530

    Time: 9 ms (60.94%) | Memory: 8.8 MB (6.60%) - LeetSync

--- minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -19,11 +19,37 @@ class Solution {
 
   }
 
+
+
+  int solvetab(vector<int>& v){
+     vector<vector<int>>dp(v.size(),vector<int>(v.size(),0));
+
+     for(int i=v.size()-1;i>=0;i--){
+       for(int j=i+2;j<v.size();j++){
+           
+     int mini = INT_MAX;
+
+    for(int k = i+1;k<j;k++){
+        mini = min(mini,v[i]*v[j]*v[k]+dp[i][k]+dp[k][j]);
+    }
+
+     dp[i][j] = mini;
+
+       }
+     }
+
+     return dp[0][v.size()-1];
+
+
+  }
+
 public:
     int minScoreTriangulation(vector<int>& values) {
 
       vector<vector<int>>dp(values.size()+1,vector<int>(values.size()+1,-1));
 
-        return solve(values,0,values.size()-1,dp);        
+        // return solve(values,0,values.size()-1,dp);  
+
+        return solvetab(values);      
     }
 };
