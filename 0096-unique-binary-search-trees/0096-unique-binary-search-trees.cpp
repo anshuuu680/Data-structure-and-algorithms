0774de8dd898f413232d2cadff5dd064507098a5
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-05 11:01:13 +0530

    Time: 3 ms (50.18%), Space: 6.2 MB (12.16%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -16,12 +16,36 @@ int solve(int n,vector<int>&dp){
         return dp[n] = ans;
 }
 
+int solvetab(int N){
+  vector<int>dp(N+1,0);
+  dp[0]=1;
+  dp[1]=1;
+
+   
+   for(int n=2;n<=N;n++){
+      int ans = 0;
+
+        for(int i=1;i<=n;i++){
+          ans+= dp[i-1] * dp[n-i];
+        }
+
+         dp[n] = ans;
+
+
+   }
+
+   return dp[N];
+
+
+}
+
 
 public:
     int numTrees(int n) {
-      vector<int>dp(n+1,-1);
+      // vector<int>dp(n+1,-1);
 
-        return solve(n,dp);
+        // return solve(n,dp);
+        return solvetab(n);
 
        
         
