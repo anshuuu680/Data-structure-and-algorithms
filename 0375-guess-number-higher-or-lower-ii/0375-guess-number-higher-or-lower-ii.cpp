c392a7a3737b19f22d9a90f5496dcc6fe7a67a39
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-05 12:17:01 +0530

    Time: 150 ms (69.85%), Space: 8.3 MB (45.55%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -16,12 +16,31 @@ class Solution {
      return dp[s][e] = mini;
  }
 
+ int solvetab(int n){
+
+      vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+
+      for(int s = n; s>=1; s--){
+          for(int e = s+1; e<=n; e++){
+
+           int mini = INT_MAX;
+
+     for(int i=s;i<=e;i++){
+         mini = min(mini,i + max(dp[s][i-1], dp[i+1][e]));
+     }
+      dp[s][e] = mini; 
+      }
+          }
+          return dp[1][n];
+ }
+
 
 public:
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
 
-        return solve(1,n,dp);
+        // return solve(1,n,dp);
+        return solvetab(n);
         
     }
 };
