3cd8ab3bc9c351c064df4f9279c214ea36cb2b13
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-05 12:09:21 +0530

    Time: 313 ms (18.00%) | Memory: 8.2 MB (45.55%) - LeetSync

---
+++ guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+
+ int solve(int s,int e, vector<vector<int>> &dp){
+     if(s>=e)
+     return 0;
+
+     if(dp[s][e]!=-1)
+     return dp[s][e];
+     
+     int mini = INT_MAX;
+
+     for(int i=s;i<=e;i++){
+         mini = min(mini,i + max(solve(s , i-1,dp) , solve(i+1 , e,dp)));
+     }
+
+     return dp[s][e] = mini;
+ }
+
+
+public:
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+
+        return solve(1,n,dp);
+        
+    }
+};
