8e3cc97cb9124bfb43c841377d6c143249664861
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-08 11:06:49 +0530

    Time: 129 ms (33.08%), Space: 67.7 MB (45.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -7,8 +7,7 @@ public:
 
         for(int i=0;i<nums.size();i++){
             max_sum += nums[i];
-            if(max_so_far<max_sum)
-                max_so_far = max_sum;
+          max_so_far = max(max_so_far,max_sum);
             if(max_sum < 0)
             max_sum =0;
         }
