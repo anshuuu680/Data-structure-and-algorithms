601bee48e42190fe9e960ee0750b123157527ac4
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-22 15:22:25 +0530

    Time: 489 ms (19.41%), Space: 17.1 MB (28.39%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -23,12 +23,35 @@ class Solution {
 
 
 
+ int solvetab(vector<int>& nums) {
+    vector<int> dp(nums.size(), INT_MAX);  // Initialize with INT_MAX
+
+    dp[nums.size() - 1] = 0;  // Set the last position as 0 since it's the end
+
+    for (int pos = nums.size() - 2; pos >= 0; pos--) {
+        int max_jump = min(pos + nums[pos], static_cast<int>(nums.size() - 1));
+        for (int i = pos + 1; i <= max_jump; i++) {
+            if (dp[i] != INT_MAX) {
+                dp[pos] = min(dp[pos], 1 + dp[i]);
+            }
+        }
+    }
+
+    return dp[0];
+}
+
+
+
+
 public:
     int jump(vector<int>& nums) {
 
-        vector<int>dp(nums.size(),-1);
+        // vector<int>dp(nums.size(),-1);
 
-        return solve(nums,0,dp);
+        // return solve(nums,0,dp);
+
+
+        return solvetab(nums);
 
         
     }
