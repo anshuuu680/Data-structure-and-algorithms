55d811ee19f7b9977fb8839b68b404ba2d2c77b6
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-23 12:11:01 +0530

    Time: 435 ms (48.50%) | Memory: 10.4 MB (65.78%) - LeetSync

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,63 @@
+class Solution {
+
+  
+
+
+  int solve(vector<int>nums,int index,int prevEle,unordered_map<string,int>&dp){
+    if(index==nums.size())
+    return 0;
+
+    string Key = to_string(index) + "#" + to_string(prevEle);
+
+     if (dp.count(Key)) {
+        return dp[Key];
+    }
+
+
+    int include=0;
+    int exclude=0;
+
+    // Include
+    if(nums[index]>prevEle)
+    include = 1 + solve(nums,index+1,nums[index],dp);
+
+    exclude = solve(nums,index+1,prevEle,dp);
+
+    return dp[Key] = max(include,exclude);
+    
+  }
+
+
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        // unordered_map<string,int>dp;
+
+      
+        // return solve(nums,0,INT_MIN,dp);
+
+
+        vector<int>dp(n,1);
+
+
+        for(int i=1;i<n;i++){
+          for(int j=0;j<i;j++){
+            if(nums[i]>nums[j])
+            dp[i] = max(dp[i],dp[j]+1);
+          }
+        }
+
+         int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        maxLen = std::max(maxLen, dp[i]);
+    }
+
+    return maxLen;
+
+
+
+
+
+    }
+};
