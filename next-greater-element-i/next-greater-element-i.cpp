c9b6fc05128d0c9f54768231c58697b075163165
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-06-24 09:33:39 +0530

    Time: 21 ms (21.31%) | Memory: 38.1 MB (6.73%) - LeetSync

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,17 +1,29 @@
 class Solution {
+
+int search(vector<int>nums,int k, int ele){
+    for(int i=k;i<nums.size();i++){
+        if(nums[i]>ele)
+        return nums[i];
+    }
+    return -1;
+}
+
+
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-         stack<int> s;
-        unordered_map<int, int> m;
-        for (int n : nums2) {
-            while (s.size() && s.top() < n) {
-                m[s.top()] = n;
-                s.pop();
+        vector<int>ans;
+        
+        for(int i=0;i<nums1.size();i++){
+            int ele = nums1[i];
+            for(int j=0;j<nums2.size();j++){
+                if(ele==nums2[j]){
+                int k = search(nums2,j,ele);
+                ans.push_back(k);
+                break;
+                }
             }
-            s.push(n);
         }
-        vector<int> ans;
-        for (int n : nums1) ans.push_back(m.count(n) ? m[n] : -1);
         return ans;
+
     }
 };
