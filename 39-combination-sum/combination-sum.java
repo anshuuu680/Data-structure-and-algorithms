4590f2b5be370a5848e7d01b84b2a974ba6c7b70
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-08-25 10:48:14 +0530

    Time: 2 ms (76.62%) | Memory: 43.9 MB (67.22%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -10,6 +10,8 @@ public class Solution {
     private void solve(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int index){
     	if(target > 0){
     		for(int i = index; i < candidates.length && target >= candidates[i]; i++){
+            if (i > index && candidates[i] == candidates[i - 1])
+                    continue;
     			cur.add(candidates[i]);
     			solve(result, cur, candidates, target - candidates[i], i);
     			cur.remove(cur.size() - 1);
