9045ee04634a873ab92f998333d2505b19ec057b
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-08-25 10:43:27 +0530

    Time: 2 ms (76.62%) | Memory: 43.9 MB (56.11%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    	  Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        solve(result, new ArrayList<Integer>(), candidates, target, 0);
+        
+        return result;
+    }
+    
+    private void solve(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int index){
+    	if(target > 0){
+    		for(int i = index; i < candidates.length && target >= candidates[i]; i++){
+    			cur.add(candidates[i]);
+    			solve(result, cur, candidates, target - candidates[i], i);
+    			cur.remove(cur.size() - 1);
+    		}
+    	}
+    	else if(target == 0 ){
+    		result.add(new ArrayList<Integer>(cur));
+    	}
+    }
+}
