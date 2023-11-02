1c0f7ac626f7f8ac375d51d584760bf0ea7ed38d
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2023-11-02 14:06:55 +0530

    Time: 2 ms (27.93%) | Memory: 40.3 MB (69.13%) - LeetSync

--- 2226-rings-and-rods/rings-and-rods.java
+++ 2226-rings-and-rods/rings-and-rods.java
@@ -1,30 +1,21 @@
 class Solution {
     public int countPoints(String rings) {
-      Set<Integer> R = new HashSet<>();
-      Set<Integer> G = new HashSet<>();
-      Set<Integer> B = new HashSet<>();
+         Map<Integer, Set<Character>> ms = new HashMap<>();
 
+        for (int i = 0; i < rings.length() - 1; i += 2) {
+            int key = rings.charAt(i + 1) - '0';
+            char value = rings.charAt(i);
+            ms.computeIfAbsent(key, k -> new HashSet<>()).add(value);
+        }
 
-      for(int i=0;i<rings.length();i+=2){
-          if(rings.charAt(i)=='R')
-          R.add(rings.charAt(i+1)- '0');
-          else if(rings.charAt(i)=='G')
-          G.add(rings.charAt(i+1)- '0');
-          else if(rings.charAt(i)=='B')
-          B.add(rings.charAt(i+1)- '0');
-      }
+        int count = 0;
+        for (Map.Entry<Integer, Set<Character> > entry : ms.entrySet()) {
+            if (entry.getValue().size() == 3) {
+                count++;
+            }
+        }
 
-
-      int ans = 0;
-      for(int i=0;i<=9;i++){
-          if(R.contains(i) && G.contains(i) && B.contains(i))
-          ans++;
-      }
-
-      return ans;
-
-
-
+        return count;
         }
     
 }
