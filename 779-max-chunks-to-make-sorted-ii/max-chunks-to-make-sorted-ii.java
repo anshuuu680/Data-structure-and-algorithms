e1fd1967fac45bb95f72f9cbd8b0718d808aa027
Anshu patidar <108322612+anshuuu680@users.noreply.github.com>
2025-01-09 12:29:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (72.50%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
         int n = arr.length;
-        int[] leftmin = new int[n];
-        int[] rightmax = new int[n];
+        int[] leftmax = new int[n];
+        int[] rightmin = new int[n];
 
-        leftmin[0] = arr[0];
+        leftmax[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            leftmin[i] = Math.max(leftmin[i-1], arr[i]);
+            leftmax[i] = Math.max(leftmax[i-1], arr[i]);
         }
 
-        rightmax[n - 1] = arr[n - 1];
+        rightmin[n - 1] = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            rightmax[i] = Math.min(rightmax[i + 1], arr[i]);
-        }
+            rightmin[i] = Math.min(rightmin[i + 1], arr[i]);
+         }
 
         int res = 0;
         for (int i = 0; i < n - 1; i++) {
-            if (leftmin[i] <= rightmax[i + 1]) res++;
+            if (leftmax[i] <= rightmin[i + 1]) res++;
         }
 
         return res + 1;
